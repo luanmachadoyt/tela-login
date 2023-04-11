e8fa4b943d0172ff0e44fcdbb81901ec0bb42b03
Luan Machado <94332377+luanmachadoyt@users.noreply.github.com>
2023-04-11 17:25:16 -0300

    Add files via upload

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,4 @@
-$( "#Clicar" ).click(function() {
-  $(".containder").css("display","flex");
+$("#Clicar").click(function () {
+  $(".containder").css("display", "flex");
+  $("#Clicar").css("display", "none");
 });
